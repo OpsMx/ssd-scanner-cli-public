6b38d5976c13b3787c023752b856aeae70d0e595
Ritik Jain <60597329+re-Tick@users.noreply.github.com>
2025-05-23 14:05:27 +0530

    Update install_ssd_scanner_cli.sh

--- install_ssd_scanner_cli.sh
+++ install_ssd_scanner_cli.sh
@@ -60,7 +60,7 @@ case "$ARCH" in
 esac
 
 # Define the URL
-URL="https://github.com/OpsMx/ssd-scanner-cli-public/releases/download/v2025.05.03-alpha2/ssd-scanner-cli-${ARCH}"
+URL="https://github.com/OpsMx/ssd-scanner-cli-public/releases/download/v2025.05.03-alpha3/ssd-scanner-cli-${ARCH}"
 
 # Download the CLI
 echo "Downloading ssd-scanner-cli for architecture: $ARCH"
